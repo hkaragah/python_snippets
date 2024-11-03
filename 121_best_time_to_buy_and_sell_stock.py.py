df08e6ebb0ae23c1b469f61401c55978bb083c59
Hossein Karagah <42470308+hkaragah@users.noreply.github.com>
2024-11-03 07:28:07 -0800

    Updated DFS and BFS implementations, added 121 practice problem

---
+++ 121_best_time_to_buy_and_sell_stock.py.py
@@ -0,0 +1,90 @@
+"""
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+"""
+
+
+
+
+def myFunc(prices:list[float]):
+    
+    buy_index= None
+    sell_index = None
+    profit = 0
+    
+    for buy in range(len(prices)-1):
+        for sell in range(buy+1, len(prices)):
+            
+            if prices[sell] - prices[buy] > profit:
+                
+                profit = prices[sell] - prices[buy]
+                buy_index = buy
+                sell_index = sell
+                
+    return profit
+
+
+
+          
+def maxProfit(prices):
+    min_price = float('inf')
+    max_profit = 0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_profit:
+            max_profit = price - min_price
+
+    return max_profit
+
+# Example usage
+print(maxProfit([7,1,5,3,6,4]))  # Output: 5
+print(maxProfit([7,6,4,3,1]))    # Output: 0
+
+        
+
+
+
+
+def main():
+    
+    prices1 = [7,1,5,3,6,4]
+    prices2 = [7,6,4,3,1]
+    
+    print(myFunc(prices1))
+    print(myFunc(prices2))
+    
+    
+    print(maxProfit(prices1))
+    print(maxProfit(prices2))
+    
+    
+
+import os
+if __name__ == "__main__":
+    os.system('cls')
+    main()
